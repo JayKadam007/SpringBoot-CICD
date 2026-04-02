aa1b37c9b121b38c11479d724bd88dee155d3949
JayKadam007 <jay007kadam@gmail.com>
2026-04-02 19:37:07 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/Controller/Partice.java
@@ -0,0 +1,24 @@
+package com.example.demo.Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping()
+public class Partice {
+
+
+    @GetMapping("/hi")
+    public String beDemo(){
+        return " hello Guys My Name Is Jay";
+    }
+
+//    git init
+//    git add README.md
+//    git commit -m "first commit"
+//    git branch -M main
+//    git remote add origin https://github.com/JayKadam007/SpringBoot-CICD.git
+//    git push -u origin main
+}
